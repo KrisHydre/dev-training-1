46980e8c9eef6c8db582985bdb0b10926eb8a3be
KrisHydre <45457995+KrisHydre@users.noreply.github.com>
2020-11-18 16:14:58 +0700

    Update tang_gia_tri_x.py

--- agorithm/tang_gia_tri_x.py
+++ agorithm/tang_gia_tri_x.py
@@ -5,9 +5,8 @@ Created on Wed Nov 18 11:08:12 2020
 
 @author: krish
 """
-
-def tang_x(x,y):
-    
+def tang_x_x(x,y):
+    buoc = 0
     # Gia su x, va y la 2 so nguyen
     if x>y:
         tang_x(y,x)
@@ -17,6 +16,7 @@ def tang_x(x,y):
             b = y-x
             for i in range (b):
                 x +=1
+                buoc +=1
             # print ('Gia tri moi cua x:', x)
             # print ('Gia tri cua y:', y)
             print ('So buoc de tang gia tri:', b)
@@ -25,10 +25,23 @@ def tang_x(x,y):
             b = y - x
             for i in range (b):
                 x +=1
-            print ('Gia tri moi cua x:', x)
-            print ('Gia tri cua y:', y)
+                buoc +=1
+            # print ('Gia tri moi cua x:', x)
+            # print ('Gia tri cua y:', y)
             print ('So buoc de tang gia tri:', b)
-                
-                
+            
+def tang_x(x,y):
+    buoc=0
+    if x > y:
+        tang_x(y,x)
+    else:
+        while y > x:
+            x *= 2
+            buoc += 1
+        for i in range (x-y):
+            x -= 1
+            buoc += 1
+        print ('Tăng giá trị của x bằng y sau ', buoc)
 if __name__== '__main__':
     tang_x(x=int(input ('So be la\n>>')), y=int(input ('So lon la\n>>')))
+    
