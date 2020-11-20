8959999e947e12764d9eda088f69d5dc3e7cb1cf
KrisHydre <45457995+KrisHydre@users.noreply.github.com>
2020-11-20 10:34:04 +0700

    Update create_db.py

--- other/nhap_du_lieu_vao_db/create_db.py
+++ other/nhap_du_lieu_vao_db/create_db.py
@@ -5,27 +5,27 @@ from mysql.connector import errorcode
 #######################################
 # CREATE THE DATABASE 
 
-## Please re-check the USER and PASSWORD before RUN the code
+## Please re-check the USER and PASSWORD before RUN the code(IN YOUR INPUT)
 
 def login_acc():
-    # CREATE A CONNECTION WITH MYSQL SERVER
+# CREATE A CONNECTION WITH MYSQL SERVER
     while True:
         try:
             User = input ('Please type the user name to login MySQL Server\n>>')
             Password = input('And the password\n>>')
-            conn = con.MySQLConnection(user=User, password=Password,
+            connects = con.MySQLConnection(user=User, password=Password,
                              host='127.0.0.1', auth_plugin='mysql_native_password')
             break
-        except mysql.connector.errorcode as err:
+        except con.Error as err:
             print ('Fail to connect to MySQL. {}.'.format (err))
             continue
-    return User, Password, conn
+    return User, Password, connects
     
 def create_database():
 # CREATE A CURSOR & DB   
     while True:   
         try:  
-            cursor = conn.cursor()
+            cursor = connects.cursor()
             create_db = [
                 'DROP DATABASE IF EXISTS customer_details',
                 'CREATE DATABASE customer_details']
@@ -36,12 +36,12 @@ def create_database():
             print ('Create the database')
             sleep(2)
             print ('Create schema successfully !')
-            cursor.close(), conn.close()
+            cursor.close(), connects.close()
             break
-        except mysql.connector.errorcode as err:
+        except con.Error as err:
             print (err)
             continue
             
 if __name__ == '__main__':
-    user, passwd, conn = login_acc()
+    user, passwd, connects = login_acc()
     create_database()
