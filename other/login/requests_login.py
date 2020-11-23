12a9e9ea6b7c71af73839dff0e1baf1870ad8c85
KrisHydre <45457995+KrisHydre@users.noreply.github.com>
2020-11-23 08:39:42 +0700

    Update requests_login.py

--- other/login/requests_login.py
+++ other/login/requests_login.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 18 15:56:05 2020
-
 @author: krish
 """
 ######################
@@ -11,6 +10,16 @@ Created on Wed Nov 18 15:56:05 2020
 
 import requests as r
 from time import sleep
+import requests.adapters as ad
+
+def check(name):
+    with open(name, 'r') as f:
+        datafile = f.readlines()
+    found = False  # This isn't really necessary
+    txt = r"class='display-name'>admin"
+    for line in datafile:
+        if txt in line:
+            print (txt)
 
 print ('Connecting..')
 for i in range (3):
@@ -21,26 +30,31 @@ login = 'http://45.79.43.178/source_carts/wordpress/wp-login.php'
 link = 'http://45.79.43.178/source_carts/wordpress/wp-admin/'
 username = 'admin'
 password = '123456aA'
-with r.Session() as req:
-    datas = {'log':username, 'pwd':password, 'wp-submit':'Log in',
-            'redirect_to':link,'testcookie':'1'}
-    req.post(login, data = datas)
-    resp = req.get (link)
+try:
+    with r.Session() as req:
+        datas = {'log':username, 'pwd':password, 'wp-submit':'Log in',
+                'redirect_to':link,'testcookie':'1'}
+        req.post(login, data = datas)
+        resp = req.get (link)
+        
+
+    if resp.status_code == 200:
+        print ('Connect to the Website successfully.')
+    header = resp.headers
+    with open ('headers.txt', 'w+') as f:
+        f.truncate()
+        for x,y in header.items():
+            f.writelines ('{}: {}'.format(x,y))
+            f.writelines('\n')
+    txt = resp.text
+
+    with open ('text.html', 'w+') as t:
+        t.truncate()
+        t.write(txt)
+    print ('The recently login user is\n>>')
+    check('text.html')
     
-
-if resp.status_code == 200:
-    print ('Connect to the Website successfully.')
-header = resp.headers
-with open ('headers.txt', 'w+') as f:
-    f.truncate()
-    for x,y in header.items():
-        f.writelines ('{}: {}'.format(x,y))
-        f.writelines('\n')
-txt = resp.text
-
-with open ('text.html', 'w+') as t:
-    t.truncate()
-    t.write(txt)
-
-
-
+except ad.ConnectionError:
+    print ('ConnectionError')
+    print ('There is no Internet. Please check the Internet \
+connection and re-run the code')
